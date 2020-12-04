c27a7e07a1941ab95d433f2cd2479c01ba3faacd
burwinliu <burwincollege@gmail.com>
2020-12-03 18:13:23 -0800

    updating server

--- raspi/main.py
+++ raspi/main.py
@@ -16,37 +16,53 @@ AWS_SECRET_ACCESS_KEY
     The secret key for your AWS account.
 
 '''
+
+ENUM = 0
+
+
 def uploadPic():
-    s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY)    
-    contents = s3.list_objects(Bucket='drawerbucket')['Contents']
-    print(contents)
+    # s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID,
+    #         aws_secret_access_key=AWS_SECRET_ACCESS_KEY)    
+    # contents = s3.list_objects(Bucket='drawerbucket')['Contents']
+    # print(contents)
 
     try:
-        enum = 0
-        s3.upload_file("./tmp/captured.png", 'drawerbucket', f'captured_{enum}')
+        # s3.upload_file("./tmp/captured.png", 'drawerbucket', f'captured_{ENUM}')
+        ENUM += 1
+        try:
+            with open("./tmp/enum", "w+") as fp 
+                # absolute file positioning 
+                fp.seek(0)  
+                
+                # to erase all data  
+                fp.truncate()  
+                fp.write(ENUM)
+        except IOError:
+
+        
         return
     except NoCredentialsError:
         print("Bad Credentials")
 
 
-def capturePic(path):
-    if os.path.isfile(path + '/captured.jpg'):
-        os.remove(path+'/captured.png')
+def capturePic():
+    if os.path.isfile('./tmp/captured.jpg'):
+        os.remove('./tmp/captured.png')
     camera = cv2.VideoCapture(0)
     return_value, image = camera.read()
-    cv2.imwrite(path + '/captured.png', image)
+    cv2.imwrite('./tmp/captured.png', image)
     del(camera)
     uploadPic()
 
 if __name__ == "__main__":
+    try:
+        with open('./tmp_enum', 'r') as fp:
+            ENUM = int(fp.readline())
     ser = serial.Serial(sys.argv[1], 9600)
     while 1: 
         s = ser.readline().decode('utf-8').strip()
         if s:
-            print(s)
             if s == "1":
                 # Take Picture on this action, and upload the item
-                print("CORRECT")
-                capturePic("./tmp")
+                capturePic()
 
