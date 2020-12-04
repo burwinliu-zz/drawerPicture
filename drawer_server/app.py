3efac8fc4d4b6e709ca2bdb55331deef7d37d3d5
burwinliu <burwincollege@gmail.com>
2020-12-03 18:11:50 -0800

    updating server

--- drawer_server/app.py
+++ drawer_server/app.py
@@ -1,10 +1,27 @@
 from flask import Flask, send_file, jsonify
+
+from secrets import *
+'''
+AWS_ACCESS_KEY_ID
+    The access key for your AWS account.
+AWS_SECRET_ACCESS_KEY
+    The secret key for your AWS account.
+
+'''
+
 app = Flask(__name__)
 
 DATA_LABELS = {'version': 0, 'labels': []}
 
 def reloadPickle():
-    pass
+    try:
+        with open('.\\tmp\\data.json', 'r') as fp:
+            DATA_LABELS = json.load(fp)
+    except IOError:
+        print("No Saved Data")
+
+def pollServer():
+    
 
 
 @app.route("/get_image")
@@ -14,10 +31,13 @@ def getImage():
 
 @app.route("/get_labels")
 def getLabels():
+
+    with open('.\\tmp\\data.json', 'w') as fp:
+        json.dump(DATA_LABELS, fp)
     return jsonify(DATA_LABELS)
 
 
 if __name__ == "__main__":
     reloadPickle()
 
-    app.run()
+    app.run()   
